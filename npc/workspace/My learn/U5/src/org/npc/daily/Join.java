c4b51afc554ddd770509d23cafd2a0fff51d84eb
Cong Zhao <1051736889@qq.com>
2017-12-02 20:45:10 +0800

    U5.2.1

---
+++ npc/workspace/My learn/U5/src/org/npc/daily/Join.java
@@ -0,0 +1,11 @@
+package org.npc.daily1;
+
+public class Join {
+	public static void main(String[]args) {
+		String s1 = new String("Let me tell you");
+		String s2 = new String("I'm a really smart guy");
+		String s  = s1 + "," + s2 + "." ;
+		System.out.println(s);
+		System.out.println("学了这种方法后，我觉得我的前一个程序相当愚蠢！！！");
+	}
+}
