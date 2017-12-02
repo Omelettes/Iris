203040f48df7bbeec635133d69963301563920a1
Cong Zhao <1051736889@qq.com>
2017-12-02 20:05:13 +0800

    Update Strings.java

--- npc/workspace/My learn/U5/src/org/npc/daily/Strings.java
+++ npc/workspace/My learn/U5/src/org/npc/daily/Strings.java
@@ -10,14 +10,14 @@ public class Strings {
 		char f[] = {'r','i','s','k'};
 		String r = new String(a),             
 		       s = new String(b,0,2),         
-			   t = new String(b,3,1),         
-			   u = new String(c,0,1);         
+		       t = new String(b,3,1),         
+		       u = new String(c,0,1);         
 		String v = new String(e,1,1),         
 		       w = new String(f,0,1),         
 		       x = new String(e,3,1);         
 		String y = new String(c,3,1),         
-			   z = new String(a,1,1),         
-			   z2= new String(d,0,1);         
+		       z = new String(a,1,1),         
+		       z2= new String(d,0,1);         
 		String z3= new String(c,2,1);         
 		System.out.print(s+t);
 		System.out.print(" ");
