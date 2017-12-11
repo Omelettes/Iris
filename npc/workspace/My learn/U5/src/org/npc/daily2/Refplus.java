e113633837073d07f3b2fa2f496a9e138618ba54
Cong Zhao <1051736889@qq.com>
2017-12-11 13:17:40 +0800

    5.4.1

---
+++ npc/workspace/My learn/U5/src/org/npc/daily2/Refplus.java
@@ -0,0 +1,16 @@
+package org.npc.daily;
+/*
+public class Refplus {
+	public static void main(String[]args) {
+		String str = "I wanna feel my heart." ;
+		char mychar= str.charAt(6);
+		System.out.println("字符串str中索引位置是6的字符为："+mychar) ;
+	}
+}
+*/
+public class Refplus{
+	public static void main(String[]args) {
+		String str1 = "Feels like we met somewhere before." ;
+		System.out.println(str1.substring(0,15));
+	}
+}
