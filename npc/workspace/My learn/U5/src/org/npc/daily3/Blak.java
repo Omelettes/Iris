293dac84b45e3f31ec7b0e9c2d52f133c2e49005
Cong Zhao <1051736889@qq.com>
2017-12-15 12:40:40 +0800

    5.4.2-5.4.5

---
+++ npc/workspace/My learn/U5/src/org/npc/daily3/Blak.java
@@ -0,0 +1,10 @@
+package org.npc.daily;
+
+public class Blak {
+	public static void main(String[]args) {
+		String str = " keep learning! " ;
+		System.out.println("字符串原来的长度是："+str.length()) ;
+		System.out.println("去掉空格后的长度是："+str.trim().length()) ;
+		System.out.println("去掉空格后的字符串副本："+str.trim());
+	}
+}
