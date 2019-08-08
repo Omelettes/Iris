151c6e8bde8e2dda89bdae7711b3053f6ad61c57
npc <1051736889@qq.com>
2019-08-08 16:13:21 +0800

    delete useless files

--- npc/workspace/My learn/U5/src/org/npc/daily1/Getlength.java
+++
@@ -1,9 +0,0 @@
-package org.npc.daily2;
-
-public class Getlength {
-	public static void main(String[]args) {
-		String str = "真是滑,天下之大稽！   啊！" ;
-		byte size  = (byte)(str.length());
-		System.out.println((float)size);
-	}
-}
--- npc/workspace/My learn/U5/src/org/npc/daily1/Ref.java
+++
@@ -1,9 +0,0 @@
-package org.npc.daily2;
-
-public class Ref {
-	public static void main(String[]args) {
-		String str = "And since birth I've been cursed with this curse to just curse" ;
-		char mychar= str.charAt(18) ;
-		System.out.println("字符串str中索引位置是18的字符为："+mychar);
-	}
-}
